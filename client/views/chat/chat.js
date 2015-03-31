c588c1eb47c4cd334c30dc3783c24867a3ee5cc4
xithan <philipp.kuehl@gmail.com>
2015-04-01 00:30:12 +0200

    moved 'leave game' button to right side

--- client/views/chat/chat.js
+++ client/views/chat/chat.js
@@ -1,3 +1,9 @@
+Template.chat.helpers({
+  inGame: function() {
+    return Players.findOne({gameId: this.gameId, userId: Meteor.userId(), robotId: {$ne:null}});
+  }
+});
+
 Template.chat.events({
   'submit form': function(event) {
     event.preventDefault();
@@ -14,7 +20,21 @@ Template.chat.events({
         $(event.target).find('[name=message]').val('');
       });
     }
-  }
+  },
+  'click .cancel': function() {
+    var game = Games.findOne(this.gameId);
+    if (game.gamePhase != GameState.PHASE.ENDED) {
+      if (confirm("If you leave, you will forfeit the game, are you sure you want to give up?")) {
+        Meteor.call('leaveGame', game._id, function(error) {
+          if (error)
+            alert(error.reason);
+          Router.go('gamelist.page');
+        });
+      }
+    } else {
+      Router.go('gamelist.page');
+    }
+  },
 });
 
 Template.chat.rendered = function() {
