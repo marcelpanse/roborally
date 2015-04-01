fc2676e629514f9186437de5417d9042445e5445
Marcel Panse <marcel.panse@gmail.com>
2015-04-01 20:50:59 +0000

    added like/share button

--- client/views/home.js
+++ client/views/home.js
@@ -1,49 +1,60 @@
-Template.home.rendered = function() {
+Template.home.rendered = function () {
   $('body').attr('id', 'page-top').attr('class', 'index');
 
   // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $(function() {
-      $('.page-scroll a').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
+  $(function () {
+    $('.page-scroll a').bind('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
   });
 
   // Highlight the top nav as scrolling occurs
   $('body').scrollspy({
-      target: '.navbar-fixed-top'
+    target: '.navbar-fixed-top'
   });
 
   // Closes the Responsive Menu on Menu Item Click
-  $('.navbar-collapse ul li a').click(function() {
-      $('.navbar-toggle:visible').click();
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
   });
 
-  var cbpAnimatedHeader = (function() {
+  if (typeof FB === "undefined") {
+    window.fbAsyncInit = initFB;
+  } else {
+    FB.XFBML.parse();
+    //Hack to make the FB like button always show..
+    Meteor.setTimeout(function() {
+      $(".fb-like").find("span").css("width", "120px").css("height", "20px");
+      $(".fb-like").find("iframe").css("width", "120px").css("height", "20px");
+    }, 2000);
+  }
+
+  var cbpAnimatedHeader = (function () {
     var docElem = document.documentElement,
-      header = document.querySelector( '.navbar-fixed-top' ),
+      header = document.querySelector('.navbar-fixed-top'),
       didScroll = false,
       changeHeaderOn = 300;
 
     function init() {
-      window.addEventListener( 'scroll', function( event ) {
-        if( !didScroll ) {
+      window.addEventListener('scroll', function (event) {
+        if (!didScroll) {
           didScroll = true;
-          setTimeout( scrollPage, 250 );
+          setTimeout(scrollPage, 250);
         }
-      }, false );
+      }, false);
     }
 
     function scrollPage() {
       var sy = scrollY();
-      if ( sy >= changeHeaderOn ) {
-        classie.add( header, 'navbar-shrink' );
+      if (sy >= changeHeaderOn) {
+        classie.add(header, 'navbar-shrink');
       }
       else {
-        classie.remove( header, 'navbar-shrink' );
+        classie.remove(header, 'navbar-shrink');
       }
       didScroll = false;
     }
@@ -56,3 +67,11 @@ Template.home.rendered = function() {
 
   })();
 };
+
+function initFB() {
+  FB.init({
+    appId: '826601937360527', // App ID from the App Dashboard
+    status: false, // check the login status upon init?
+    xfbml: true  // parse XFBML tags on this page?
+  });
+}
