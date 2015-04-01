0425c25e737bb91b4ceeac3c4a84278c97e2d97e
Marcel Panse <marcel.panse@gmail.com>
2015-04-01 18:48:01 +0000

    sitemap and disconnect detection fixes

--- server/cron.js
+++ server/cron.js
@@ -1,7 +1,6 @@
 Meteor.startup(function () {
   var everyMinute = new Cron(function() {
     //runs every minute and cleans up abandoned games.
-    console.log("running cron every minute");
     var openGames = Games.find({started: false}).fetch();
     openGames.forEach(function(game) {
       if (!Meteor.users.findOne(game.userId).status.online) {
@@ -21,28 +20,46 @@ Meteor.startup(function () {
       var players = Players.find({gameId: game._id}).fetch();
       var playersOnline = 0;
       var lastManStanding = false;
+      var nrOfPlayers = players.length;
+      var nrOfPlayersChecked = 0;
+
       players.forEach(function(player) {
         if (!Meteor.users.findOne(player.userId).status.online) {
           //wait couple of seconds and re-check before deleting the game, to make sure it wasn't a refresh or temporary.
           console.log("Found disconnected player, waiting couple of seconds: " + game._id);
           Meteor.setTimeout(function() {
             if (!Meteor.users.findOne(player.userId).status.online) {
+              //really offline
               var debug_info = "Forfeitting game with disconnected player: " + game._id;
               player.chat('disconnected and left the game', debug_info);
+              nrOfPlayersChecked++;
+            } else {
+              //did come online
+              lastManStanding = player;
+              playersOnline++;
+              nrOfPlayersChecked++;
             }
           }, 5000);
         } else {
           lastManStanding = player;
           playersOnline++;
+          nrOfPlayersChecked++;
         }
-
       });
-      if (playersOnline === 0) {
-        Games.update(game._id, {$set: {gamePhase: GameState.PHASE.ENDED, winner: "Nobody"}});
-      } else if (playersOnline < game.min_player) {
-        Games.update(game._id, {$set: {gamePhase: GameState.PHASE.ENDED, winner: lastManStanding.name}});
-      }
-
+      //this will wait untill all checks are finished.
+      var handle = Meteor.setInterval(function() {
+        console.log("waiting for player to come back online..");
+        if (nrOfPlayersChecked >= nrOfPlayers) {
+          console.log("all players checked, players online: ", playersOnline);
+          Meteor.clearInterval(handle);
+          if (playersOnline === 0) {
+            Games.update(game._id, {$set: {gamePhase: GameState.PHASE.ENDED, winner: "Nobody"}});
+          } else if (playersOnline == 1 && game.min_player > 1) {
+            Games.update(game._id, {$set: {gamePhase: GameState.PHASE.ENDED, winner: lastManStanding.name}});
+          }
+          //else do nothing, game still in progress..
+        }
+      }, 1000);
     });
 
     //cleanup inactive users
